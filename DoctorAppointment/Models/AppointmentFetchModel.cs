8837665aeb704f308d79810f8eca59479f8385d7
Sandeep Sagar Panjala <>
2023-07-21 10:37:04 -0500

    Completed filter model and controller

---
+++ DoctorAppointment/Models/AppointmentFetchModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DoctorAppointment.Models
+{
+	public class AppointmentFetchModel
+	{
+        public int AppointmentId { get; set; }
+        public string PatientName { get; set; }
+        public string DoctorName { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public TimeSpan AppointmentTime { get; set; }
+        public string Notes { get; set; }
+    }
+}
+
