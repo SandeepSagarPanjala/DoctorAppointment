72f776a951a0f96fddf3fd3dcad6300a10acc7ea
Sareedy-Shyam <140186123+Sareedy-Shyam@users.noreply.github.com>
2023-07-21 10:41:38 -0500

    completed prescription models

---
+++ DoctorAppointment/Models/PrescriptionFilterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DoctorAppointment.Models
+{
+	public class PrescriptionFilterModel
+	{
+        public int AppointmentId { get; set; }
+        public string MedicationName { get; set; }
+    }
+}
+
