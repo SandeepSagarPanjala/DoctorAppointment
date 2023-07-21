7e70b1b2668c48c55f9239f778c04bf8f59ecc9e
Sandeep Sagar Panjala <>
2023-07-21 10:55:55 -0500

    Completed doctor patients controller

---
+++ DoctorAppointment/Controllers/DoctorPatientController.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+using Model = DoctorAppointment.Models.PatientModel;
+
+namespace DoctorAppointment.Controllers;
+
+[ApiController]
+[Route("doctor-patient")]
+public class DoctorPatientController : ControllerBase
+{
+    private readonly NpgsqlConnection connection;
+
+    public DoctorPatientController(IConfiguration _configuration)
+    {
+        string connectionString = _configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+        this.connection = new NpgsqlConnection(connectionString);
+    }
+
+    ~DoctorPatientController()
+    {
+        if(this.connection != null)
+        {
+            this.connection.Close();
+        }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var query = @$"SELECT p.* FROM ""Appointment"" a 
+                        JOIN ""Doctor"" d on d.""DoctorId"" = a.""DoctorId""
+                        JOIN ""Patient"" p on p.""PatientId"" = a.""PatientId""
+                        WHERE ""DoctorId"" = {id}";
+        var record = await connection.QuerySingleAsync<Model>(query);
+        return Ok(record);
+    }
+}
+
